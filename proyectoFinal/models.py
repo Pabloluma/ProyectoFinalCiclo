f56f72a0ccda57b7f3112cc86980a17e3590d3cc
Pablo <pablolupionmarquez@gmail.com>
2025-03-21 11:00:38 +0100

    Clase caracteristicas, tipo bicicletas y publico o privada la ruta

--- proyectoFinal/models.py
+++ proyectoFinal/models.py
@@ -16,11 +16,17 @@ class Rutas(models.Model):
     descenso = models.IntegerField()
     dureza = models.CharField(max_length=45)
     imagen = models.ImageField(upload_to="proyectoFinal", null=True, blank=True)
+    publico = models.BooleanField(default=False)
     # imagen = models.CharField(max_length=100, blank=True, null=True)
     idUsuario = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
         return f"{self.titulo} - {self.idUsuario.first_name}"
+
+
+class tiposBicicletas(models.Model):
+    nombre_tipo = models.CharField(max_length=50, unique=True)
+    tipoNeumatico = models.CharField(max_length=45)
 
 
 class caracteristicas(models.Model):
@@ -29,5 +35,6 @@ class caracteristicas(models.Model):
     peso = models.DecimalField(max_digits=5, decimal_places=2)
     estado = models.PositiveSmallIntegerField()
     suelo = models.PositiveSmallIntegerField()
-    tipo_bici = models.PositiveSmallIntegerField()
+    # tipo_bici = models.PositiveSmallIntegerField()
+    tipo_bici = models.ForeignKey(tiposBicicletas, on_delete=models.CASCADE, null=True, blank=True)
     usuario_id = models.OneToOneField(User, on_delete=models.CASCADE)
