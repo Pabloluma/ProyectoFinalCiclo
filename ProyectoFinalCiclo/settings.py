8477bb0e3c7e90950aa4429f365a45406ca80679
Pablo <pablolupionmarquez@gmail.com>
2025-05-09 17:22:00 +0200

    permitir acceso a todos los usuarios

--- ProyectoFinalCiclo/settings.py
+++ ProyectoFinalCiclo/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-$puccep&!n#b_-6ur938n+k*h)unx*)z)@j1s-378w6m%^j1)4
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
